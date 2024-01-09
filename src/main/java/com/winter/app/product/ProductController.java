361e4f80bf51d40de5dabe3f708bc66ac4bf7e66
beomkim7 <beomkim7@naver.com>
2024-01-09 17:47:17 +0900

    4

--- src/main/java/com/winter/app/product/ProductController.java
+++ src/main/java/com/winter/app/product/ProductController.java
@@ -5,37 +5,38 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping(value = "/products/*")
 public class ProductController {
+	
+	
 	private ProductDAO productDAO;
-
-	public ProductController () {
-		this.productDAO = new ProductDAO();
-	}
 	
-	
-	@RequestMapping(value = "list", method =RequestMethod.GET)
-	public String list(Model model) throws Exception{
 
+	
+	@RequestMapping(value = "list",method = RequestMethod.GET)
+	public String list(HttpServletRequest request) throws Exception {
 		
-		List<ProductDTO> list = productDAO.getlist();
-		model.addAttribute("list", list);
-		return "products/list";
+		List<ProductDTO> ar = productDAO.getlist();
+		request.setAttribute("list", ar);
+		
+		return "list";
 	}
+	
 	@RequestMapping(value = "detail",method = RequestMethod.GET)
-	public String detail(Model model, HttpServletRequest request) throws Exception {
-		Long id = Long.parseLong(request.getParameter("id")); 
-
-		
+	public String detail(HttpServletRequest request) throws Exception {
 		ProductDTO productDTO = new ProductDTO();
-		productDTO.setProductNum(id);
-		productDTO = productDAO.getdetail(productDTO);
-		model.addAttribute("dto", productDTO);
+		String id = request.getParameter("productNum");
+		
+		productDTO.setProductNum(Long.parseLong(id));
+		productDTO=productDAO.getdetail(productDTO);
+		
+		request.setAttribute("dto",productDTO);
+		
 		return "products/detail";
+		
 	}
 }
